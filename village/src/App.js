de0470d4c31c51197d15bb392c14d90ea60d8947
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-25 10:15:35 -0700

    Router set up

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import API from './api.js';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -67,12 +68,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm 
-          addSmurf={this.addSmurf} 
-          handleInputChange={this.handleInputChange} 
-          newSmurf={this.state.newSmurf} 
+        <Route 
+          path="/smurf-form"
+          render={props => <SmurfForm {...props}
+            addSmurf={this.addSmurf} 
+            handleInputChange={this.handleInputChange} 
+            newSmurf={this.state.newSmurf} 
+          />}
         />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route 
+          path="/"
+          render={(props) => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
       </div>
     );
   }
